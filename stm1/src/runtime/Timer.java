a1fa31331d69fd253511b843a501f683148808b8
Frank Alexander <kraemer.frank@gmail.com>
2013-08-14 10:34:43 +0200

    Improved timer.

--- stm1/src/runtime/Timer.java
+++ stm1/src/runtime/Timer.java
@@ -2,31 +2,40 @@ package runtime;
 
 import java.util.TimerTask;
 
-public class Timer extends java.util.Timer {
+/**
+ * This is a timer that can be used within a state machine to dispatch
+ * an event after a given delay. This timer is based on a <code>java.util.Timer</code>, 
+ * so that the scheduler does not have to maintain its own queue of timers.
+ */
+public class Timer {
 	
 	private String timerId;
 	private Scheduler scheduler;
 	private long delay;
+	private TimerTask task;
+	private java.util.Timer timer;
 	
 	public Timer(String timerId) {
-		super(timerId);
+		this.timer = new java.util.Timer(timerId);
 		this.timerId = timerId;
 	}
 	
 	public void start(final Scheduler scheduler, long delay) {
-		this.schedule(new TimerTask() {
+		task = new TimerTask() {
 			public void run() {
 				scheduler.addToQueueFirst(timerId);
 			}
-		}, delay);
+		};
+		timer.schedule(task, delay);
 	}
 	
 	public void stop() {
-		super.cancel();
+		task.cancel();
+		task = null;
 	}
 	
 	public void restart() {
-		super.cancel();
+		this.stop();
 		this.start(scheduler, delay);
 	}
 	
